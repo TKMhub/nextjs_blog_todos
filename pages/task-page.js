ce079c9a9b4b3dceab7d048bd1396aecc18dbe16
TKMhub <111867865+TKMhub@users.noreply.github.com>
2023-02-25 16:44:27 +0900

    complete commit

--- pages/task-page.js
+++ pages/task-page.js
@@ -4,6 +4,8 @@ import Link from "next/link";
 import { getAllTasksData } from "../lib/tasks";
 import Task from "../components/Task";
 import useSWR from "swr";
+import StateContextProvider from "../context/StateContext";
+import TaskForm from "../components/taskForm";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 const apiUrl = `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/list-task/`;
@@ -18,34 +20,39 @@ export default function TaskPage({ staticfilterdTasks }) {
 
   useEffect(() => {
     mutate();
-  }, []);
+  }, [mutate]);
 
   return (
-    <Layout title="Task page">
-      <ul>
-        {fillteredTasks &&
-          fillteredTasks.map((task) => <Task key={task.id} task={task} />)}
-      </ul>
-      <Link href={"/main-page"}>
-        <div className="flex cursor-pointer mt-12">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5"
-            />
-          </svg>
-          <span>Back to main page</span>
-        </div>
-      </Link>
-    </Layout>
+    <StateContextProvider>
+      <Layout title="Task page">
+        <TaskForm taskCreated={mutate} />
+        <ul>
+          {fillteredTasks &&
+            fillteredTasks.map((task) => (
+              <Task key={task.id} task={task} taskDeleted={mutate} />
+            ))}
+        </ul>
+        <Link href={"/main-page"}>
+          <div className="flex cursor-pointer mt-12">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5"
+              />
+            </svg>
+            <span>Back to main page</span>
+          </div>
+        </Link>
+      </Layout>
+    </StateContextProvider>
   );
 }
 
